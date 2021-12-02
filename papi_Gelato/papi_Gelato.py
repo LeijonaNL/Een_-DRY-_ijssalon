5a3656bf507d8ee3db3b44fef48da2b91e1e2878
Lennert <99069053@mydavinci.nl>
2021-12-02 13:29:26 +0100

    Papi Gelato

---
+++ papi_Gelato/papi_Gelato.py
@@ -0,0 +1,125 @@
+# Papi Gelato
+
+# Importing time from device operating system.
+import time, os
+
+# Assigning preset input values to prevent error.
+aantalBolletjes = 0 # int
+hoorntjeBakje = 'Preset' # str
+doorgaanStoppen = 'Preset' # str
+
+# Function for clearing screen (CLS), parameter is time until execution after initiation.
+def clearScreen(sleepTime):
+    time.sleep(sleepTime)
+    os.system("cls")
+
+# Function for canceling the program if requested, trigger: cancelProgram
+def cancelProgramRequest():
+    if aantalBolletjes == 'CANCELPROGRAM' or hoorntjeBakje == 'CANCELPROGRAM' or doorgaanStoppen == 'CANCELPROGRAM':
+        print('Het programma is gestopt.')
+        clearScreen(2)
+        exit()
+
+
+# 
+def vraagAantalBolletjes(): # Main function 1
+    print("Welkom bij Papi Gelato, je mag alle smaken kiezen zo lang het maar vanille ijs is.")
+    global aantalBolletjes # This variable is now globally usable.
+    repeat = True
+    #
+    while repeat:
+        repeat = False
+        aantalBolletjes = input("Hoeveel bolletjes wilt U? ").upper()
+        cancelProgramRequest()
+        if aantalBolletjes != 'CANCELPROGRAM':
+            aantalBolletjes = int(aantalBolletjes)
+        if 0 < aantalBolletjes <= 3:
+            clearScreen(1)
+        elif 3 < aantalBolletjes <= 8:
+            print(f"Dan krijgt u van mij een bakje met {aantalBolletjes} bolletjes.")
+            clearScreen(3)
+        elif 8 < aantalBolletjes:
+            print("Sorry, zulke grote bakken hebben we niet.")
+            clearScreen(2)
+            repeat = True
+        else:
+            print("Sorry dat snap ik niet...")
+            clearScreen(2)
+            repeat = True
+
+
+#
+def vraagHoorntjeBakje(): # Main function 2
+    global hoorntjeBakje # This variable is now globally usable.
+    repeat = True
+    #
+    while repeat:
+        repeat = False
+        if 3 < aantalBolletjes:
+            hoorntjeBakje = 'bakje'
+        elif aantalBolletjes == 1:
+            hoorntjeBakje = input(f"Wilt U dit bolletje in een A) een hoorntje of B) een bakje? ").upper()
+            cancelProgramRequest()
+            if hoorntjeBakje == 'A':
+                clearScreen(1)
+                hoorntjeBakje = 'hoorntje'
+            elif hoorntjeBakje == 'B':
+                clearScreen(1)
+                hoorntjeBakje = 'bakje'
+            else:
+                print("Sorry, dat snap ik niet...")
+                clearScreen(2)
+                repeat = True
+        elif 1 < aantalBolletjes <= 3:
+            hoorntjeBakje = input(f"Wilt U deze {aantalBolletjes} bolletje(s) in A) een hoorntje of B) een bakje? ").upper()
+            cancelProgramRequest()
+            if hoorntjeBakje == 'A':
+                clearScreen(1)
+                hoorntjeBakje = 'hoorntje'
+            elif hoorntjeBakje == 'B':
+                clearScreen(1)
+                hoorntjeBakje = 'bakje'
+            else:
+                print("Sorry, dat snap ik niet...")
+                clearScreen(2)
+                repeat = True
+
+
+#
+def vraagDoorgaanStoppen(): # Main function 3
+    global doorgaanStoppen # This variable is now globally usable.
+    repeat = True
+    #
+    while repeat:
+        repeat = False
+        if aantalBolletjes == 1:
+            doorgaanStoppen = input(f"Hier is uw {hoorntjeBakje} met {aantalBolletjes} bolletje. Wilt u nog meer bestellen? (Y/N) ").upper()
+        else:
+            doorgaanStoppen = input(f"Hier is uw {hoorntjeBakje} met {aantalBolletjes} bolletjes. Wilt u nog meer bestellen? (Y/N) ").upper()
+        cancelProgramRequest()
+        if doorgaanStoppen == 'Y':
+            clearScreen(1)
+        elif doorgaanStoppen == 'N':
+            print("Bedankt en tot ziens!")
+            clearScreen(2)
+        else:
+            print("Sorry, dat snap ik niet...")
+            clearScreen(2)
+            repeat = True
+
+
+# Main function layout.
+def papiGelato():
+    repeat = True
+    #
+    while repeat:
+        repeat = False
+        vraagAantalBolletjes()
+        vraagHoorntjeBakje()
+        vraagDoorgaanStoppen()
+        if doorgaanStoppen == 'Y': # If Y, the main function will be repeated.
+            repeat = True
+
+# Main function execution.
+clearScreen(1)
+papiGelato()
